b368789f6e5c8504dd893c4787ae3dfd8a0d0868
dotslashlevi <76601635+dotslashlevi@users.noreply.github.com>
2022-09-09 09:03:52 -0400

    Updated and made more secure and private

--- macos-debloater.sh
+++ macos-debloater.sh
@@ -14,12 +14,8 @@ echo "Disabling the SMB protocol."
 sudo defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool TRUE
 echo "Success."
 
-echo "Disbling FileVault."
-sudo fdesetup disable
-echo "Success."
-
 echo "Disabling Crash Reporting, Call History, Diagnostics, FTP, Spindump, Helpd, and Location Services." 
-SERVICES="com.apple.geod com.apple.helpd com.apple.spindump com.apple.ftp-proxy com.apple.metadata.mds.spindump com.apple.CallHistoryPluginHelper com.apple.ap.adprivacyd com.apple.ReportPanic com.apple.ReportCrash com.apple.ReportCrash.Self com.apple.DiagnosticReportCleanup"
+SERVICES="com.apple.geod com.apple.helpd com.apple.spindump com.apple.ftp-proxy com.apple.metadata.mds.spindump com.apple.ReportPanic com.apple.ReportCrash com.apple.ReportCrash.Self com.apple.DiagnosticReportCleanup"
 for val in ${SER[*]}
 do
   sudo launchctl disable $val
