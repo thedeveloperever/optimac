41449a652ad732d5927a135b139e530942de5cf4
dotslashlevi <76601635+dotslashlevi@users.noreply.github.com>
2022-10-03 15:24:13 -0400

    Update macos-debloater.sh

--- macos-debloater.sh
+++ macos-debloater.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
 echo "Disabling all animations."
-sudo defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
-sudo defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
 sudo defaults write com.apple.finder DisableAllAnimations -bool true
+sudo defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
 sudo defaults write com.apple.dock launchanim -bool false
 sudo defaults write com.apple.dock expose-animation-duration -float 0.1
+sudo defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
 sudo defaults write com.apple.Dock autohide-delay -float 0
-sudo defaults write -g QLPanelAnimationDuration -float 0
+sudo defaults write QLPanelAnimationDuration -float 0
+killall dock
 echo "Success."
+
+echo "Removing log files."
+sudo rm -rf /private/var/log/*.log
+sudo rm -rf /private/var/log/asl/*.asl
+
+echo "
 
 echo "Disabling the SMB protocol."
 sudo defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool TRUE
@@ -20,10 +27,11 @@ echo "smb_neg=smb2_only" | sudo tee -a /etc/nsmb.conf
 echo "Success."
 
 echo "Disabling Crash Reporting, Diagnostics, FTP, Spindump, Helpd, and Location Services." 
-SERVICES="com.apple.geod com.apple.helpd com.apple.spindump com.apple.ftp-proxy com.apple.metadata.mds.spindump com.apple.ReportPanic com.apple.ReportCrash com.apple.ReportCrash.Self com.apple.DiagnosticReportCleanup"
-for val in ${SER[*]}
+SERVICES="com.apple.analyticsd com.apple.VoiceOver com.apple.amp.mediasharingd com.apple.mediaanalysisd com.apple.mediaremoteagent com.apple.photoanalysisd com.apple.java.InstallOnDemand com.apple.voicememod com.apple.geod com.apple.locate com.apple.locationd com.apple.netbiosd com.apple.recentsd com.apple.suggestd com.apple.ftp-proxy com.apple.TrustEvaluationAgent com.apple.spindump com.apple.metadata.mds.spindump com.apple.ReportPanic com.apple.ReportCrash com.apple.ReportCrash.Self com.apple.DiagnosticReportCleanup"
+for service in ${SERVICES[*]}
 do
-  sudo launchctl disable $val
+  sudo launchctl disable $SERVICE
+  echo "[Complete] Disabled $SERVICE."
 done
 echo "Success."
 
